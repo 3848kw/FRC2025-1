a67f89ebb9bc12f9e8238cb313ac0b80805bfe45
3848kw <166737520+3848kw@users.noreply.github.com>
2025-01-31 13:31:24 -0600

    new

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -27,7 +27,7 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  */
 public class RobotContainer {
  private SendableChooser<Command> autoChooser = new SendableChooser<>();
-   g
+   
    // The robot's subsystems and commands are defined here...
    private final SwerveSubsystem drivebase = new SwerveSubsystem();
    // Replace with CommandPS4Controller or CommandJoystick if needed
