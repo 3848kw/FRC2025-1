0df7d489c3f34bb12009e5a106520f6badc054a6
3848kw <dbreyer@kw.k12.mn.us>
2025-02-14 20:50:07 -0600

    e

--- src/main/java/frc/robot/subsystems/climber.java
+++ src/main/java/frc/robot/subsystems/climber.java
@@ -2,7 +2,6 @@ package frc.robot.subsystems;
 import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.kForward;
 import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.kReverse;
 
-import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
@@ -10,6 +9,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 /** A hatch mechanism actuated by a single {@link DoubleSolenoid}. */
 public class climber extends SubsystemBase {
+  @SuppressWarnings("unused")
   private final Compressor m_compressor = new Compressor(1,PneumaticsModuleType.CTREPCM);
 
   private final DoubleSolenoid m_hatchSolenoid =
--- src/main/java/frc/robot/subsystems/elevator.java
+++ src/main/java/frc/robot/subsystems/elevator.java
@@ -1,12 +1,10 @@
 package frc.robot.subsystems;
-import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
@@ -16,6 +14,7 @@ import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
 public class elevator {
 private SparkMax elevatorMotor;
 private SparkMaxConfig motorConfig;
+@SuppressWarnings("unused")
 private SparkClosedLoopController closedLoopController;
 private RelativeEncoder encoder;
 
